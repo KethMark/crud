3f557562ddb124398f6a19bb81828bc03bc34581
Keth Mark <aseronberou@gmail.com>
2025-08-30 12:41:22 +0800

    It should not create another tab

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="/Information"
-          target="_blank"
           rel="noopener noreferrer"
         >
           <Image
